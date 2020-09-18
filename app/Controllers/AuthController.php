091ad55d09db02bb2a021789c6def2a1f788876e
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-09-18 12:42:46 +0200

    fix correct logout

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,8 +2,16 @@
 
 namespace App\Controllers;
 
+use CQ\Helpers\Session;
+use CQ\Config\Config;
 use CQ\Controllers\Auth;
 
 class AuthController extends Auth
 {
+    public function logout($msg = 'logout')
+    {
+        Session::destroy();
+
+        return $this->redirect('https://auth.castelnuovo.xyz/oauth2/authorize?client_id=' . Config::get('auth.id'));
+    }
 }
